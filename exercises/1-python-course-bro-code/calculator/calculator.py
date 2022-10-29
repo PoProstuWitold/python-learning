810352ec66c30707e4de7a1032d4bfbbdfbe3aee
PoProstuWitold <witekzawada@o2.pl>
2022-10-29 19:54:21 +0200

    Bro Code: Finished

---
+++ exercises/1-python-course-bro-code/calculator/calculator.py
@@ -0,0 +1,106 @@
+# **************************************************************
+# Python Calculator
+# **************************************************************
+from tkinter import *
+
+def button_press(num):
+
+    global equation_text
+
+    equation_text = equation_text + str(num)
+
+    equation_label.set(equation_text)
+
+def equals():
+
+    global equation_text
+
+    try:
+
+        total = str(eval(equation_text))
+
+        equation_label.set(total)
+
+        equation_text = total
+
+    except SyntaxError:
+
+        equation_label.set("syntax error")
+
+        equation_text = ""
+
+    except ZeroDivisionError:
+
+        equation_label.set("arithmetic error")
+
+        equation_text = ""
+
+def clear():
+
+    global equation_text
+
+    equation_label.set("")
+
+    equation_text = ""
+
+
+window = Tk()
+window.title("Calculator program")
+window.geometry("500x500")
+
+equation_text = ""
+
+equation_label = StringVar()
+
+label = Label(window, textvariable=equation_label, font=('consolas',20), bg="white", width=24, height=2)
+label.pack()
+
+frame = Frame(window)
+frame.pack()
+
+btns = []
+btns_nmbr = -1
+
+for x in range(0, 3):
+
+    for y in range(0, 3):
+        btns_nmbr += 1
+
+        btns.append(Button(frame, text=9 - btns_nmbr, height=4, width=9, font=35,
+                           command=lambda btns_nmbr=btns_nmbr: button_press(9 - btns_nmbr)))
+        btns[btns_nmbr].grid(row=2-x, column=2-y)
+
+
+button0 = Button(frame, text=0, height=4, width=9, font=35,
+                 command=lambda: button_press(0))
+button0.grid(row=3, column=0)
+
+plus = Button(frame, text='+', height=4, width=9, font=35,
+                 command=lambda: button_press('+'))
+plus.grid(row=0, column=3)
+
+minus = Button(frame, text='-', height=4, width=9, font=35,
+                 command=lambda: button_press('-'))
+minus.grid(row=1, column=3)
+
+multiply = Button(frame, text='*', height=4, width=9, font=35,
+                 command=lambda: button_press('*'))
+multiply.grid(row=2, column=3)
+
+divide = Button(frame, text='/', height=4, width=9, font=35,
+                 command=lambda: button_press('/'))
+divide.grid(row=3, column=3)
+
+equal = Button(frame, text='=', height=4, width=9, font=35,
+                 command=equals)
+equal.grid(row=3, column=2)
+
+decimal = Button(frame, text='.', height=4, width=9, font=35,
+                 command=lambda: button_press('.'))
+decimal.grid(row=3, column=1)
+
+clear = Button(window, text='clear', height=4, width=12, font=35,
+                 command=clear)
+clear.pack()
+
+window.mainloop()
